8052263fa9f70a1075a641d7131ad3b10cf1d248
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-02-06 04:15:13 +0100

    Specify the ajax dataType in order to avoid jQuery confusion.

--- code.js
+++ code.js
@@ -1,23 +1,26 @@
 
-$.get('site.min.json')
-.success(function(data) {
-  var body = $('#data').find('tbody')
-    , data = JSON.parse(data);
+$.ajax({
+  type: 'get',
+  url: 'site.min.json',
+  dataType: 'json',
+  success: function(data) {
+    var body = $('#data').find('tbody');
 
-  for (var i = 0; i < data.length; i++) {
-    var e = data[i];
-    body.append($('<tr>')
-      .append($('<td>').text(e.cvar))
-      .append($('<td>').text(e.value.default))
-      .append($('<td>').text(e.value.min))
-      .append($('<td>').text(e.value.max))
-      .append($('<td>').text(e.type))
-      .append($('<td>').text(e.category))
-      .append($('<td>').text(e.description))
-    );
+    for (var i = 0; i < data.length; i++) {
+      var e = data[i];
+      body.append($('<tr>')
+        .append($('<td>').text(e.cvar))
+        .append($('<td>').text(e.value.default))
+        .append($('<td>').text(e.value.min))
+        .append($('<td>').text(e.value.max))
+        .append($('<td>').text(e.type))
+        .append($('<td>').text(e.category))
+        .append($('<td>').text(e.description))
+      );
+    }
+    $('#info').text("successfully loaded");
   }
-  $('#info').text("successfully loaded");
 })
-.error(function(data) {
+.error(function (data) {
   $('#info').text("failed to load");
 })
